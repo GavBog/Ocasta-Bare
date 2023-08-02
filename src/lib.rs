eceb01fe41ac866af68b5a3577338c6a6f34c547
GavBog <me@gbogie.com>
2023-08-02 19:46:36 -0400

    Organization & Fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod errors;
+pub mod util;
 pub mod websocket;
--- src/main.rs
+++ src/main.rs
@@ -6,92 +6,13 @@ use axum::{
     routing::{any, get},
     Router,
 };
-use ocastaproxy::{errors, websocket};
+use ocastaproxy::{
+    errors,
+    util::{join_headers, split_headers},
+    websocket,
+};
 use serde_json::Value;
 use std::net::SocketAddr;
-
-fn split_headers(headers: HeaderMap) -> HeaderMap {
-    let mut output = headers.clone();
-
-    if let Some(value) = headers.get("x-bare-headers") {
-        if let Ok(value) = value.to_str() {
-            if value.len() > 3072 {
-                output.remove("x-bare-headers");
-
-                let mut split = 0;
-
-                for i in (0..value.len()).step_by(3072) {
-                    let part = &value[i..i + 3072];
-
-                    let id = split;
-                    output.insert(
-                        if let Ok(key) =
-                            HeaderName::from_bytes(format!("x-bare-headers-{}", id).as_bytes())
-                        {
-                            key
-                        } else {
-                            continue;
-                        },
-                        if let Ok(value) = HeaderValue::from_str(format!("{};", part).as_str()) {
-                            value
-                        } else {
-                            continue;
-                        },
-                    );
-                    split += 1;
-                }
-            }
-        }
-    }
-
-    output
-}
-
-fn join_headers(headers: HeaderMap) -> Result<HeaderValue, ()> {
-    if headers.contains_key("x-bare-headers") {
-        return Ok(headers
-            .get("x-bare-headers")
-            .unwrap_or(&HeaderValue::from_static("[]"))
-            .clone());
-    }
-
-    let mut new_headers = HeaderMap::new();
-    for (key, value) in headers.iter() {
-        if !key.as_str().starts_with("x-bare-headers-") {
-            continue;
-        }
-        new_headers.insert(key, value.clone());
-    }
-
-    if new_headers.len() > 0 {
-        let mut join = vec![];
-        for (key, value) in headers.iter() {
-            if !value.to_str().unwrap_or_default().starts_with(';') {
-                return Err(());
-            }
-
-            let id = if let Ok(id) = key.as_str().replace("x-bare-headers-", "").parse::<usize>() {
-                id
-            } else {
-                return Err(());
-            };
-
-            join[id] = value.to_str().unwrap_or_default().replace(";", "");
-
-            new_headers.remove(key);
-        }
-
-        let output = if let Ok(output) = HeaderValue::from_str(join.join("").as_str()) {
-            output
-        } else {
-            return Err(());
-        };
-
-        return Ok(output);
-    } else {
-        return Err(());
-    }
-}
 
 async fn proxy(
     headers: HeaderMap,
@@ -114,7 +35,7 @@ async fn proxy(
     let bare_headers = if let Ok(bare_headers) = join_headers(headers.clone()) {
         bare_headers
     } else {
-        return errors::error_response(StatusCode::BAD_REQUEST).into_response();
+        HeaderValue::from_static("[]")
     };
 
     let bare_headers =
@@ -182,9 +103,7 @@ async fn proxy(
         return errors::error_response(StatusCode::BAD_REQUEST).into_response();
     };
 
-    let response = client.execute(request).await;
-
-    let response = if let Ok(response) = response {
+    let response = if let Ok(response) = client.execute(request).await {
         response
     } else {
         return errors::error_response(StatusCode::BAD_REQUEST).into_response();
