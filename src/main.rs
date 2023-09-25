fee1b6cb428bf7e60190de4e408ec7353d42109d
Avad3 <65318266+Avad3@users.noreply.github.com>
2023-09-25 00:51:01 -0400

    Replace main.rs with bin/ocasta-bare.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use argh::{FromArgs, from_env};
 use axum::{
     routing::{any, get},
     Router,
@@ -5,13 +6,37 @@ use axum::{
 use ocastabare::{util::index, v3};
 use std::net::SocketAddr;
 
+#[derive(FromArgs)]
+/// Bare server init
+struct Init {
+    /// the bare server directory, defaults to /
+    #[argh(option, short = 'd', default = "String::from(\"/\")")]
+    directory: String,
+
+    /// the listening host, defaults to 0.0.0.0
+    #[argh(option, short = 'h', default = "String::from(\"0.0.0.0\")")]
+    host: String,
+
+    /// the port number, defaults to 80
+    #[argh(option, short = 'p', default = "80")]
+    port: u16
+}
+
 #[tokio::main]
 async fn main() {
+    let init: Init = from_env();
+    let mut addr_tuple = ([0, 0, 0, 0], init.port);
+
+    for (i, num) in init.host.split('.').enumerate() {
+        addr_tuple.0[i] = num.parse::<u8>().unwrap();
+    }
+
     let app = Router::new()
-        .route("/", get(index))
+        .route(&init.directory, get(index))
         .route("/v3/", any(v3::proxy));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let addr = SocketAddr::from(addr_tuple);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
