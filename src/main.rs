2d49c1cebf3f7ca95c373a1cc25fa70e710f25b5
Avad3 <65318266+Avad3@users.noreply.github.com>
2023-09-25 11:04:47 -0400

    Add CLI and memory usage (#2)
    * Add CLI and memory usage
    
    * remove json depend and static index.json file
    
    * Replace main.rs with bin/ocasta-bare.rs
    
    * Prepend Directory to Routes as well.
    
    * a single space (makes it look prettier)
    
    ---------
    
    Co-authored-by: GavBog <me@gbogie.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use argh::{from_env, FromArgs};
 use axum::{
     routing::{any, get},
     Router,
@@ -5,13 +6,41 @@ use axum::{
 use ocastabare::{util::index, v3};
 use std::net::SocketAddr;
 
+#[derive(FromArgs)]
+/// Bare server init
+struct Init {
+    /// the bare server directory, defaults to /
+    #[argh(option, short = 'd', default = "String::from(\"/\")")]
+    directory: String,
+
+    /// the listening host, defaults to 0.0.0.0
+    #[argh(option, short = 'h', default = "String::from(\"0.0.0.0\")")]
+    host: String,
+
+    /// the port number, defaults to 80
+    #[argh(option, short = 'p', default = "80")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(index))
-        .route("/v3/", any(v3::proxy));
+    let init: Init = from_env();
+    let mut addr_tuple = ([0, 0, 0, 0], init.port);
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    for (i, num) in init.host.split('.').enumerate() {
+        addr_tuple.0[i] = num.parse::<u8>().unwrap();
+    }
+
+    let app = Router::new().route(&init.directory, get(index)).route(
+        format!(
+            "{}/v3/",
+            init.directory.strip_suffix("/").unwrap_or_default()
+        )
+        .as_str(),
+        any(v3::proxy),
+    );
+
+    let addr = SocketAddr::from(addr_tuple);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
--- src/util.rs
+++ src/util.rs
@@ -2,6 +2,8 @@ use axum::{
     body::Body,
     http::{HeaderMap, HeaderName, HeaderValue, Response},
 };
+use memory_stats::memory_stats;
+use serde_json::{json, to_string_pretty};
 
 pub async fn index() -> Response<Body> {
     let mut headers = HeaderMap::new();
@@ -15,8 +17,28 @@ pub async fn index() -> Response<Body> {
         headers.insert(key, HeaderValue::from_static("*"));
     }
 
+    let json = json!(
+        {
+            "versions": ["v3"],
+            "language": "Rust",
+            "memoryUsage": f64::from((memory_stats().unwrap().physical_mem as f64 / 1024.0 / 1024.0) * 100.0).round() / 100.0,
+            "maintainer": {
+              "email": "you@example.com",
+              "website": "https://www.example.com/"
+            },
+            "project": {
+              "name": "Ocasta-Bare",
+              "description": "Rust TOMP implementation",
+              "email": "hello@projectocasta.org",
+              "website": "https://www.projectocasta.org/",
+              "repository": "https://github.com/Project-Ocasta/Ocasta-Bare",
+              "version": "1.0.0"
+            }
+          }
+    );
+
     let mut res = Response::default();
-    *res.body_mut() = include_str!("../static/index.json").into();
+    *res.body_mut() = to_string_pretty(&json).unwrap_or_default().into();
     *res.headers_mut() = headers;
     res
 }
